b692b0e0adbec08899dc7c3c7da51ac7816168af
leebert <brenner.lee@gmail.com>
2020-08-12 21:13:20 -0500

    case study page layout : in progress

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# naptime-site
- Lee Brenner's portfolio site and exploratory playground.
+# Naptime, yay!
+Lee's portfolio and exploratory playground.
+
+## Naming Conventions 
+I'm loosely following the [BEM Methodology](https://webdesign.tutsplus.com/articles/an-introduction-to-the-bem-methodology--cms-19403).
+
+**Filename Prefixes**
+- `c-` Component
+- `h-` Helper
+- `l-` Layout
+- `s-` Style 
+- `js-` Javascript **(redundant, but reinforces the convention)**
+
+**Other**
+- In Javascript files I prefix everything with `js_[x]_` where `[x]` is the first one or two letters of the filename. I do this so that if I reference a variable or function in a different file I know who is responsible for it.
+- In HMTL files I prefix elements that are modified by Javascript with a `js_`. 
--- js/js-case-studies.js
+++ js/js-case-studies.js
@@ -1,4 +1,8 @@
 
+var j_cs_bigpicture;
+var j_cs_spacer;
+var j_cs_footer;
+
 window.addEventListener('load', js_cs_load);
 
 function js_cs_load() {
@@ -6,4 +10,18 @@ function js_cs_load() {
         document.getElementById("c-case-study-transition-shim").style.backgroundColor = "white";
     }
     document.getElementById("c-case-study-transition-shim").style.opacity = 0;
+    j_cs_bigpicture = document.getElementsByClassName("c-cs-bigpicture-container")[0];
+    j_cs_spacer = document.getElementsByClassName("c-cs-asidespacer")[0];
+    j_cs_footer = document.getElementsByClassName("c-cs-footer")[0];
+    window.addEventListener("resize", js_cs_handleResize);
+    js_cs_handleResize();
+}
+
+function js_cs_handleResize(event) {
+    var h = window.innerHeight;
+    var bp = j_cs_bigpicture.getBoundingClientRect();
+    var ft = j_cs_footer.getBoundingClientRect();
+    var actual = h - bp.top * 1.35 - bp.height - ft.height;
+    var smallest = bp.height + ft.height - bp.top * 1.35;
+    j_cs_spacer.style.height = actual < smallest ? "0" : actual + "px";
 }
