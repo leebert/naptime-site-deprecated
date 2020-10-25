5ccf25750b4969a1227b31fe10e47c4933807bac
leebert <brenner.lee@gmail.com>
2020-10-24 22:29:28 -0500

    updated about and added transition

--- js/js-common.js
+++ js/js-common.js
@@ -1,4 +1,5 @@
 var js_cmn_AboutDiv;
+var js_cmn_AboutModalDiv;
 
 function js_cmn_HandleScroll(){
     var w = window.innerHeight;
@@ -14,24 +15,41 @@ function js_cmn_HandleScroll(){
     }
 }
 
-async function js_cmn_FetchHtmlAsText(url) {
-    return await (await fetch(url)).text();
+function js_cmn_applyBlur(parent) {
+    var children = parent.children;
+    for (var i = 0; i < children.length; i++) {
+        children[i].classList.remove("blurOut");
+        children[i].classList.add("blurIn");
+    }
+    js_cmn_AboutModalDiv.classList.remove("slideOut");
+    js_cmn_AboutModalDiv.classList.add("slideIn");
 }
 
-async function js_cmn_LoadAbout() {
+function js_cmn_removeBlur(parent) {
+    var children = parent.children;
+    for (var i = 0; i < children.length; i++) {
+        children[i].classList.add("blurOut");
+        children[i].classList.remove("blurIn");
+    }
+    js_cmn_AboutModalDiv.classList.add("slideOut");
+    js_cmn_AboutModalDiv.classList.remove("slideIn");
+}
+
+function js_cmn_LoadAbout() {
+    var body = document.getElementsByTagName("BODY")[0];
     js_cmn_AboutDiv = document.createElement("div");
     js_cmn_AboutDiv.classList.add("about");
-    var body = document.getElementsByTagName("BODY")[0];
+    js_cmn_AboutDiv.classList.add("fadeIn");
     body.appendChild(js_cmn_AboutDiv);
     body.classList.add("modal-open");
     
-    var about = document.createElement("div");
-    about.classList.add("about__modal");
-    js_cmn_AboutDiv.appendChild(about);
+    js_cmn_AboutModalDiv = document.createElement("div");
+    js_cmn_AboutModalDiv.classList.add("about__modal");
+    js_cmn_AboutDiv.appendChild(js_cmn_AboutModalDiv);
 
     var container = document.createElement("div");
     container.classList.add("about__content");
-    about.appendChild(container);
+    js_cmn_AboutModalDiv.appendChild(container);
 
     var i = document.createElement("img");
     i.src = "assets/animated_headshot.gif";
@@ -43,22 +61,29 @@ async function js_cmn_LoadAbout() {
     container.appendChild(copy);
 
     var h = document.createElement("h1");
-    h.innerHTML = "👋 I'm Lee. I lead teams, direct initiatives and ship products.";
+    h.innerHTML = "👋 I'm Lee. I lead teams, direct initiatives, and ship products.";
     copy.appendChild(h);
     var p = document.createElement("p");
-    p.innerHTML = "This portfolio is a collection of stories about the things I love to do. I talk about the imperfect process of making things, finding insights in ambiguity, collaborating with enthusiastic people, and helping others do their best work.<br><br>My know-how comes from hands-on experience as an agency and start-up owner, a creative leader, a teacher, a developer, and a designer.";
+    p.innerHTML = "You are looking at a list of some things I’ve made over the years. What’s important to know is that every single thing on this list exists, or once existed...people have used, paid for, learned from, and added to their life experience with the items on this list.<br><br>These things exist because of thoughtful exploration, hard work, and collaboration with others.";
     copy.appendChild(p);
 
     var close = document.createElement("a");
     close.innerHTML = "back to the stuff";
     close.href = "#";
     close.addEventListener("click", js_cmn_CloseAbout);
-    about.appendChild(close);
+    js_cmn_AboutModalDiv.appendChild(close);
+
+    js_cmn_applyBlur(body);
 }
 
 function js_cmn_CloseAbout() {
-    document.getElementsByTagName("BODY")[0].removeChild(js_cmn_AboutDiv);
-    document.getElementsByTagName("BODY")[0].classList.remove("modal-open");
+    var body = document.getElementsByTagName("BODY")[0];
+    js_cmn_removeBlur(body);
+    js_cmn_AboutDiv.classList.add("fadeOut");
+    setTimeout(function() { 
+        document.getElementsByTagName("BODY")[0].removeChild(js_cmn_AboutDiv);
+        document.getElementsByTagName("BODY")[0].classList.remove("modal-open");
+    }, 300);
 }
 
 function js_cmn_AssignClick() {
