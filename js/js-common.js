bf3d7f2ecaddaa255f060f7a077b8435aa174873
leebert <brenner.lee@gmail.com>
2020-11-19 11:42:20 -0600

    implemented a lightbox feature to view images full-screen

--- js/js-common.js
+++ js/js-common.js
@@ -1,12 +1,65 @@
 var js_cmn_AboutDiv;
 var js_cmn_AboutModalDiv;
+var js_cmn_LightBoxDiv;
+var js_cmn_LightBoxImg;
 
 function js_cmn_SetUpCommonStuff() {
     document.addEventListener("scroll", js_cmn_HandleScroll);
     window.addEventListener("resize", js_cmn_HandleResize);
     js_cmn_HandleScroll();
-    // js_cmn_AssignClick();
     js_cmn_SetupLoad();
+}
+
+function js_cmn_SetUpLightBox() {
+    const imgs = document.getElementsByTagName("IMG");
+    for(var i = 0; i < imgs.length; i++) {
+        imgs[i].addEventListener("click", js_cmn_ShowLightBox);
+    }
+}
+
+function js_cmn_ShowLightBox(event) {
+    var body = document.body;
+    js_cmn_LightBoxDiv = document.createElement("div");
+    js_cmn_LightBoxDiv.classList.add("lightbox");
+    js_cmn_LightBoxDiv.classList.add("fadeIn");
+    js_cmn_LightBoxDiv.style.height = body.clientHeight + "px";
+    js_cmn_LightBoxDiv.style.width = body.clientWidth + "px";
+    body.appendChild(js_cmn_LightBoxDiv);
+    body.classList.add("modal-open");
+    js_cmn_LightBoxDiv.addEventListener("click", js_cmn_HideLightBox);
+    js_cmn_LightBoxImg = document.createElement("img");
+    js_cmn_LightBoxImg.classList.add("lightbox__image");
+    js_cmn_LightBoxImg.src = event.target.src;
+    js_cmn_LightBoxImg.alt = event.target.alt;
+    js_cmn_SetBoundingBox(js_cmn_LightBoxImg, body, event.target);
+    js_cmn_LightBoxDiv.appendChild(js_cmn_LightBoxImg);
+    js_cmn_applyBlur(body, js_cmn_LightBoxImg);
+}
+
+function js_cmn_HideLightBox() {
+    if (!document.body.classList.contains("modal-open")) { return; }
+    var body = document.body;
+    js_cmn_removeBlur(body, js_cmn_LightBoxImg);
+    js_cmn_LightBoxDiv.classList.add("fadeOut");
+    setTimeout(function() { 
+        document.body.removeChild(js_cmn_LightBoxDiv);
+        document.body.classList.remove("modal-open");
+    }, 300);
+}
+
+function js_cmn_SetBoundingBox(item, container, sourceItem) {
+    var w = container.clientWidth * 0.95;
+    var h = w * (sourceItem.height/sourceItem.width);        ;
+    if (h + 64 > container.clientHeight) {
+        h = container.clientHeight * 0.95 - 64;
+        w = h * (sourceItem.width/sourceItem.height);        
+    }
+    const l = (container.clientWidth - w)/2;
+    const t = document.documentElement.scrollTop + (window.innerHeight-h)/2 - 32;
+    item.style.top = t + "px";
+    item.style.left = l + "px";
+    item.style.width = w + "px";
+    item.style.height = h + "px";
 }
 
 function js_cmn_HandleScroll() {
@@ -15,6 +68,7 @@ function js_cmn_HandleScroll() {
 
 function js_cmn_HandleResize() {
     js_cmn_HandleScrollIndicatorDisplay();
+    js_cmn_HideLightBox();
 }
 
 function js_cmn_HandleScrollIndicatorDisplay() {
@@ -38,24 +92,24 @@ function js_cmn_AssignClick() {
     }
 }
 
-function js_cmn_applyBlur(parent) {
+function js_cmn_applyBlur(parent, slider) {
     var children = parent.children;
     for (var i = 0; i < children.length; i++) {
         children[i].classList.remove("blurOut");
         children[i].classList.add("blurIn");
     }
-    js_cmn_AboutModalDiv.classList.remove("slideOut");
-    js_cmn_AboutModalDiv.classList.add("slideIn");
+    slider.classList.remove("slideOut");
+    slider.classList.add("slideIn");
 }
 
-function js_cmn_removeBlur(parent) {
+function js_cmn_removeBlur(parent, slider) {
     var children = parent.children;
     for (var i = 0; i < children.length; i++) {
         children[i].classList.add("blurOut");
         children[i].classList.remove("blurIn");
     }
-    js_cmn_AboutModalDiv.classList.add("slideOut");
-    js_cmn_AboutModalDiv.classList.remove("slideIn");
+    slider.classList.add("slideOut");
+    slider.classList.remove("slideIn");
 }
 
 function js_cmn_LoadAbout() {
@@ -96,12 +150,12 @@ function js_cmn_LoadAbout() {
     close.addEventListener("click", js_cmn_CloseAbout);
     js_cmn_AboutModalDiv.appendChild(close);
 
-    js_cmn_applyBlur(body);
+    js_cmn_applyBlur(body, js_cmn_AboutModalDiv);
 }
 
 function js_cmn_CloseAbout() {
     var body = document.body;
-    js_cmn_removeBlur(body);
+    js_cmn_removeBlur(body, js_cmn_AboutModalDiv);
     js_cmn_AboutDiv.classList.add("fadeOut");
     setTimeout(function() { 
         document.body.removeChild(js_cmn_AboutDiv);
--- js/js-details.js
+++ js/js-details.js
@@ -4,4 +4,5 @@ window.addEventListener('load', js_d_load);
 function js_d_load() {
     document.getElementById("js_return").addEventListener("click", js_cmn_HandleNav);
     js_cmn_SetUpCommonStuff();
+    js_cmn_SetUpLightBox();
 }
