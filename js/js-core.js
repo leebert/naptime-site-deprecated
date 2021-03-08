c5e52e047875b817f89be36860bc64e245658add
leebert <brenner.lee@gmail.com>
2021-03-08 13:05:46 -0600

    fixed broken url

--- js/js-core.js
+++ js/js-core.js
@@ -1,7 +1,7 @@
 // structure is: link title, resource type, resource url, design, code, words, year
 var js_c_data = [
     {title: "An augmented reality game", type: "page", url: "stackimalsar.html", design: true, code: true, words: false, year: 2021},
-    {title: "A sound visualizer", type: "page", url: "funsize/vibes.html", design: true, code: true, words: false, year: 2021},
+    {title: "A sound visualizer", type: "page", url: "funsize/vibez.html", design: true, code: true, words: false, year: 2021},
     {title: "An iOS Scouting App for Directors & Cinematographers", type: "page", url: "cinescope.html", design: true, code: true, words: false, year: 2020},
     {title: "A SaaS Platform for Data Scientists", type: "page", url: "alegion-saas.html", design: true, code: false, words: true, year: 2020},
     {title: "A Video Annotation Tool for Data Labelers", type: "page", url: "alegion-va.html", design: true, code: false, words: false, year: 2020},
